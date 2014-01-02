a4c2dfbef41e7d71b87b92180328a560689c6861
Frank Schlegel <frank.schlegel@student.hpi.uni-potsdam.de>
2014-01-02 19:38:50 +0100

    Added an action bar with first icons indicating the step direction (+/-)

--- src/mtg-counter.c
+++ src/mtg-counter.c
@@ -2,6 +2,7 @@
 
 
 #define LIFE_DEFAULT 20
+#define LIFE_STEP_DEFAULT -1
 #define REPEATING_CLICK_INTERVAL 500
 
 
@@ -10,9 +11,15 @@ static Window* window;
 static TextLayer* text_layer_life_opponent;
 static TextLayer* text_layer_life_player;
 
+static ActionBarLayer* action_bar_layer;
+static GBitmap* action_icon_plus;
+static GBitmap* action_icon_minus;
+
 
 static int life_opponent = LIFE_DEFAULT;
 static int life_player = LIFE_DEFAULT;
+
+static short life_step = LIFE_STEP_DEFAULT;
 
 
 static void update_opponent_life_counter() {
@@ -27,18 +34,27 @@ static void update_player_life_counter() {
   text_layer_set_text(text_layer_life_player, text);
 }
 
+static void update_action_bar() {
+  GBitmap* icon = life_step < 0 ? action_icon_minus : action_icon_plus;
+
+  action_bar_layer_set_icon(action_bar_layer, BUTTON_ID_UP, icon);
+  action_bar_layer_set_icon(action_bar_layer, BUTTON_ID_DOWN, icon);
+}
+
 
 static void select_click_handler(ClickRecognizerRef recognizer, void* context) {
-  //TODO
+  // toggle step direction
+  life_step *= -1;
+  update_action_bar();
 }
 
 static void up_repeating_click_handler(ClickRecognizerRef recognizer, void* context) {
-  --life_opponent;
+  life_opponent += life_step;
   update_opponent_life_counter();
 }
 
 static void down_repeating_click_handler(ClickRecognizerRef recognizer, void* context) {
-  --life_player;
+  life_player += life_step;
   update_player_life_counter();
 }
 
@@ -52,13 +68,24 @@ static void window_load(Window* window) {
   Layer* window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer_life_opponent = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounds.size.w, 60 } });
+  // initialize the action bar
+  action_bar_layer = action_bar_layer_create();
+  // load the icons
+  action_icon_plus = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ACTION_ICON_PLUS);
+  action_icon_minus = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ACTION_ICON_MINUS);
+  update_action_bar();
+  // associate the action bar with the window
+  action_bar_layer_add_to_window(action_bar_layer, window);
+  // set the click config provider
+  action_bar_layer_set_click_config_provider(action_bar_layer, click_config_provider);
+
+  text_layer_life_opponent = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounds.size.w - ACTION_BAR_WIDTH, 60 } });
   text_layer_set_text_alignment(text_layer_life_opponent, GTextAlignmentCenter);
   text_layer_set_font(text_layer_life_opponent, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));
   update_opponent_life_counter();
   layer_add_child(window_layer, text_layer_get_layer(text_layer_life_opponent));
 
-  text_layer_life_player = text_layer_create((GRect) { .origin = { 0, 80 }, .size = { bounds.size.w, 60 } });
+  text_layer_life_player = text_layer_create((GRect) { .origin = { 0, 80 }, .size = { bounds.size.w - ACTION_BAR_WIDTH, 60 } });
   text_layer_set_text_alignment(text_layer_life_player, GTextAlignmentCenter);
   text_layer_set_font(text_layer_life_player, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));
   update_player_life_counter();
@@ -73,7 +100,7 @@ static void window_unload(Window* window) {
 static void init(void) {
   window = window_create();
 
-  window_set_click_config_provider(window, click_config_provider);
+  // window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
