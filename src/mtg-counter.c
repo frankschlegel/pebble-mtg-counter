596b11381e2263694880d066ea671c6cf252796e
Frank Schlegel <frank.schlegel@student.hpi.uni-potsdam.de>
2014-01-02 18:35:03 +0100

    Basic UI with two text layers

--- src/mtg-counter.c
+++ src/mtg-counter.c
@@ -1,49 +1,82 @@
 #include <pebble.h>
 
-static Window *window;
-static TextLayer *text_layer;
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+#define LIFE_DEFAULT 20
+
+
+static Window* window;
+
+static TextLayer* text_layer_life_opponent;
+static TextLayer* text_layer_life_player;
+
+
+static int life_opponent = LIFE_DEFAULT;
+static int life_player = LIFE_DEFAULT;
+
+
+static void update_opponent_life_counter() {
+  static char text[5];
+  snprintf(text, sizeof(text), "%d", life_opponent);
+  text_layer_set_text(text_layer_life_opponent, text);
 }
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
+static void update_player_life_counter() {
+  static char text[5];
+  snprintf(text, sizeof(text), "%d", life_player);
+  text_layer_set_text(text_layer_life_player, text);
 }
 
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
+
+static void select_click_handler(ClickRecognizerRef recognizer, void* context) {
+  //TODO
 }
 
-static void click_config_provider(void *context) {
+static void up_click_handler(ClickRecognizerRef recognizer, void* context) {
+  //TODO
+}
+
+static void down_click_handler(ClickRecognizerRef recognizer, void* context) {
+  //TODO
+}
+
+static void click_config_provider(void* context) {
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
   window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
-static void window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
+static void window_load(Window* window) {
+  Layer* window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  text_layer_life_opponent = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounds.size.w, 60 } });
+  text_layer_set_text_alignment(text_layer_life_opponent, GTextAlignmentCenter);
+  text_layer_set_font(text_layer_life_opponent, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
+  update_opponent_life_counter();
+  layer_add_child(window_layer, text_layer_get_layer(text_layer_life_opponent));
+
+  text_layer_life_player = text_layer_create((GRect) { .origin = { 0, 80 }, .size = { bounds.size.w, 60 } });
+  text_layer_set_text_alignment(text_layer_life_player, GTextAlignmentCenter);
+  text_layer_set_font(text_layer_life_player, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
+  update_player_life_counter();
+  layer_add_child(window_layer, text_layer_get_layer(text_layer_life_player));
 }
 
-static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+static void window_unload(Window* window) {
+  text_layer_destroy(text_layer_life_opponent);
+  text_layer_destroy(text_layer_life_player);
 }
 
 static void init(void) {
   window = window_create();
+
   window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
   });
-  const bool animated = true;
-  window_stack_push(window, animated);
+
+  window_stack_push(window, true /*animated*/);
 }
 
 static void deinit(void) {
