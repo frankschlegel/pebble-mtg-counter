7330e98149c7969179edcf27fc7a03be78fef790
Lysann Schlegel <lysann.kessler@gmail.com>
2014-01-07 22:47:53 +0100

    add configuration UI dummy, copied from Bert Freudenberg's PebbleONE

---
+++ src/mtg-counter.js
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2013 Bert Freudenberg
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ *
+ * Original from https://github.com/bertfreudenberg/PebbleONE.
+ * Modified for pebble-mtg-counter.
+ *
+ */
+
+// the config dict is sent as app message to the watch
+var SECONDS_MODE_NEVER    = 0,
+    SECONDS_MODE_IFNOTLOW = 1,
+    SECONDS_MODE_ALWAYS   = 2,
+    BATTERY_MODE_NEVER    = 0,
+    BATTERY_MODE_IF_LOW   = 1,
+    BATTERY_MODE_ALWAYS   = 2;
+    DATE_MODE_NEVER       = 0,
+    DATE_MODE_ALWAYS      = 1;
+    BLUETOOTH_MODE_NEVER  = 0;
+    BLUETOOTH_MODE_IFOFF  = 1;
+    BLUETOOTH_MODE_ALWAYS = 2;
+    GRAPHICS_MODE_NORMAL  = 0;
+    GRAPHICS_MODE_INVERT  = 1;
+    CONNLOST_MODE_IGNORE  = 0;
+    CONNLOST_MODE_WARN    = 1;
+
+var config = {
+    seconds_mode:   SECONDS_MODE_ALWAYS,
+    battery_mode:   BATTERY_MODE_IF_LOW,
+    date_mode:      DATE_MODE_ALWAYS,
+    bluetooth_mode: BLUETOOTH_MODE_NEVER,
+    graphics_mode:  GRAPHICS_MODE_NORMAL,
+    connlost_mode:  CONNLOST_MODE_IGNORE,
+};
+
+// config_html will be assigned the contents of config.html during build (See /wscript).
+var config_html; 
+
+function send_config_to_pebble() {
+    console.log("sending config " + JSON.stringify(config)); 
+    Pebble.sendAppMessage(config,
+        function ack(e) { console.log("Successfully delivered message " + JSON.stringify(e.data)); },
+        function nack(e) { console.log("Unable to deliver message " + JSON.stringify(e)); });
+}
+
+// read config from persistent storage
+Pebble.addEventListener('ready', function () {
+    var json = window.localStorage.getItem('config');
+    if (typeof json === 'string') {
+        config = JSON.parse(json);
+        console.log("loaded config " + JSON.stringify(config));
+    }
+});
+
+// got message from pebble
+Pebble.addEventListener('appmessage', function(e) {
+    console.log("got message " + JSON.stringify(e.payload));
+    if (e.payload.request_config)
+        send_config_to_pebble();
+});
+
+// open config window
+Pebble.addEventListener('showConfiguration', function () {
+    var html = config_html.replace('"CONFIG_JSON_REPLACED_AT_RUNTIME"', JSON.stringify(config), 'g');
+    Pebble.openURL('data:text/html,' + encodeURIComponent(html + '<!--.html'));
+});
+
+// store config and send to watch
+Pebble.addEventListener('webviewclosed', function (e) {
+    if (e.response && e.response.length) {
+        config = JSON.parse(e.response);
+        console.log("storing config " + e.response);
+        window.localStorage.setItem('config', e.response);
+        send_config_to_pebble();
+    }
+});
--- wscript
+++ wscript
@@ -1,24 +1,41 @@
-
 #
-# This file is the default set of rules to compile a Pebble project.
+# waf build script for the pebble app.
+# https://code.google.com/p/waf/
 #
-# Feel free to customize this to your needs.
+# The build porcess has been customized
+# to embed the HTML configuration UI into the javascript.
+# https://github.com/bertfreudenberg/PebbleONE
 #
 
 top = '.'
 out = 'build'
 
-def options(ctx):
-    ctx.load('pebble_sdk')
+def options(context):
+    context.load('pebble_sdk')
 
-def configure(ctx):
-    ctx.load('pebble_sdk')
+def configure(context):
+    context.load('pebble_sdk')
 
-def build(ctx):
-    ctx.load('pebble_sdk')
+def build(context):
+    context.load('pebble_sdk')
 
-    ctx.pbl_program(source=ctx.path.ant_glob('src/**/*.c'),
-                    target='pebble-app.elf')
+    context.pbl_program(
+        source=context.path.ant_glob('src/**/*.c'),
+        target='pebble-app.elf'
+    )
+    source_js_file = 'src/mtg-counter.js'
+    config_html_js_var = 'config_html'
+    config_html_file = 'src/config.html'
 
-    ctx.pbl_bundle(elf='pebble-app.elf',
-                   js=ctx.path.ant_glob('src/js/**/*.js'))
+    # generate config.js from config.html by escaping every line and quotes
+    config_html_node = context.path.make_node(config_html_file)
+    config_js_node  = context.path.get_bld().make_node('src/js/config.js')
+    context(rule='(echo '+config_html_js_var+'= && sed "s/\'/\\\\\\\'/g;s/^/\\\'/;s/$/\\\' +/" ${SRC} && echo "\'\';") > ${TGT}', source=config_html_node, target=config_js_node)
+
+    # make pebble-js-app.js by appending config.js to the source javascript file
+    source_js_node = context.path.make_node(source_js_file)
+    build_js_node  = context.path.get_bld().make_node('src/js/pebble-js-app.js')
+    context(rule='cat ${SRC} > ${TGT}', source=[source_js_node, config_js_node], target=build_js_node)
+
+    # bundle the app with the native program and the concatenated javascript
+    context.pbl_bundle(elf='pebble-app.elf', js=build_js_node)
