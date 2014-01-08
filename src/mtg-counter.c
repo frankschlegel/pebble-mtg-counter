938149f3e9f5119e79d44488a986e548188d3554
Frank Schlegel <frank.schlegel@student.hpi.uni-potsdam.de>
2014-01-08 22:43:22 +0100

    Next version of dark UI

--- src/mtg-counter.c
+++ src/mtg-counter.c
@@ -35,6 +35,7 @@ static AppTimer* timer_orientation_check;
 static Window* main_window;
 
 static Layer* background_layer;
+static Layer* action_bar_background_layer;
 static Layer* game_score_opponent_background_layer;
 static Layer* game_score_player_background_layer;
 static Layer* game_score_draw_background_layer;
@@ -102,19 +103,19 @@ static void update_player_life_counter() {
 }
 
 static void update_games_won_counter_opponent() {
-  static char text[2];
+  static char text[3];
   snprintf(text, sizeof(text), "%d", games_won_opponent);
   text_layer_set_text(text_layer_games_won_opponent, text);
 }
 
 static void update_games_won_counter_player() {
-  static char text[2];
+  static char text[3];
   snprintf(text, sizeof(text), "%d", games_won_player);
   text_layer_set_text(text_layer_games_won_player, text);
 }
 
 static void update_games_draw_counter() {
-  static char text[2];
+  static char text[3];
   snprintf(text, sizeof(text), "%d", games_draw);
   text_layer_set_text(text_layer_games_draw, text);
 }
@@ -279,8 +280,16 @@ static void game_score_background_layer_update(Layer* me, GContext* ctx) {
   GRect frame = layer_get_frame(me);
   frame.origin = (GPoint){ .x = 0, .y = 0 };
 
+  graphics_context_set_stroke_color(ctx, GColorWhite);
+  graphics_draw_round_rect(ctx, frame, 3);
+}
+
+static void action_bar_background_layer_update(Layer* me, GContext* ctx) {
+  GRect frame = layer_get_frame(me);
+  frame.origin = (GPoint){ .x = 0, .y = 0 };
+
   graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_rect(ctx, frame, 3, me == game_score_draw_background_layer ? GCornersAll : GCornersLeft);
+  graphics_fill_rect(ctx, frame, 4, GCornersLeft);
 }
 
 
@@ -299,6 +308,14 @@ static void main_window_load(Window* window) {
   action_icon_plus = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ACTION_ICON_PLUS);
   action_icon_minus = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ACTION_ICON_MINUS);
   action_icon_toggle = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ACTION_ICON_TOGGLE);
+
+  // init action bar background layer
+  action_bar_background_layer = layer_create((GRect) {
+    .origin = { bounds.size.w - ACTION_BAR_WIDTH - 1, 2 },
+    .size = { ACTION_BAR_WIDTH + 1, bounds.size.h - 4 },
+  });
+  layer_set_update_proc(action_bar_background_layer, action_bar_background_layer_update);
+  layer_add_child(window_layer, action_bar_background_layer);
 
   // init life score layers
   int score_width = 80;
@@ -332,17 +349,19 @@ static void main_window_load(Window* window) {
   GRect game_score_opponent_frame = (GRect) {
     .origin = {
       score_layer_life_opponent_frame.origin.x - 20, 
-      score_layer_life_opponent_frame.origin.y + score_layer_life_opponent_frame.size.h - 25
+      score_layer_life_opponent_frame.origin.y + score_layer_life_opponent_frame.size.h - 26
     },
-    .size = {20, 25},
+    .size = {23, 26},
   };
   game_score_opponent_background_layer = layer_create(game_score_opponent_frame);
   layer_set_update_proc(game_score_opponent_background_layer, game_score_background_layer_update);
   layer_add_child(window_layer, game_score_opponent_background_layer);
 
-  text_layer_games_won_opponent = text_layer_create((GRect) { .origin = {2, 0}, .size = {16, 18} });
+  text_layer_games_won_opponent = text_layer_create((GRect) { .origin = {2, 1}, .size = {16, 18} });
   text_layer_set_text_alignment(text_layer_games_won_opponent, GTextAlignmentCenter);
   text_layer_set_font(text_layer_games_won_opponent, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_background_color(text_layer_games_won_opponent, GColorBlack);
+  text_layer_set_text_color(text_layer_games_won_opponent, GColorWhite);
   update_games_won_counter_opponent();
   layer_add_child(game_score_opponent_background_layer, text_layer_get_layer(text_layer_games_won_opponent));
 
@@ -353,22 +372,27 @@ static void main_window_load(Window* window) {
   layer_set_update_proc(game_score_player_background_layer, game_score_background_layer_update);
   layer_add_child(window_layer, game_score_player_background_layer);
 
-  text_layer_games_won_player = text_layer_create((GRect) { .origin = {2, 0}, .size = {16, 18} });
+  text_layer_games_won_player = text_layer_create((GRect) { .origin = {2, 1}, .size = {16, 18} });
   text_layer_set_text_alignment(text_layer_games_won_player, GTextAlignmentCenter);
   text_layer_set_font(text_layer_games_won_player, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_background_color(text_layer_games_won_player, GColorBlack);
+  text_layer_set_text_color(text_layer_games_won_player, GColorWhite);
   update_games_won_counter_player();
   layer_add_child(game_score_player_background_layer, text_layer_get_layer(text_layer_games_won_player));
 
   GRect game_score_draw_frame = game_score_opponent_frame;
   game_score_draw_frame.origin.y = game_score_player_frame.origin.y - (game_score_player_frame.origin.y - game_score_opponent_frame.origin.y) / 2;
+  game_score_draw_frame.size.w = 20;
 
   game_score_draw_background_layer = layer_create(game_score_draw_frame);
   layer_set_update_proc(game_score_draw_background_layer, game_score_background_layer_update);
   layer_add_child(window_layer, game_score_draw_background_layer);
 
-  text_layer_games_draw = text_layer_create((GRect) { .origin = {2, 0}, .size = {16, 18} });
+  text_layer_games_draw = text_layer_create((GRect) { .origin = {2, 1}, .size = {16, 18} });
   text_layer_set_text_alignment(text_layer_games_draw, GTextAlignmentCenter);
   text_layer_set_font(text_layer_games_draw, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_background_color(text_layer_games_draw, GColorBlack);
+  text_layer_set_text_color(text_layer_games_draw, GColorWhite);
   update_games_draw_counter();
   layer_add_child(game_score_draw_background_layer, text_layer_get_layer(text_layer_games_draw));
 }
